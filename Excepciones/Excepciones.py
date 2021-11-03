384c1fe3f019cecf5e3a0b8fdca2c8edbf2a1f05
FranRV <franratvar7@gmail.com>
2021-11-03 15:30:12 +0100

    Generadores

---
+++ Excepciones/Excepciones.py
@@ -0,0 +1,21 @@
+# Excepciones a errores inesperados
+# Try - Except
+
+def division(num1, num2):
+    try:
+        return num1 / num2
+    except ZeroDivisionError:
+        print("No se puede dividir entre 0")
+        return "Operacion erronea"
+
+
+sw = False
+
+while not sw:
+    try:
+        op1 = int(input("Operador 1: "))
+        op2 = int(input("Operador 2: "))
+        sw = True
+        print("Resultado de la division: ", division(op1, op2))
+    except ValueError:
+        print("Introduce valores numericos")
