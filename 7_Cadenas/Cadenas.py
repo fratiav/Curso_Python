6b7d0ef846b82548909560670f8650b68b58748f
FranRV <franratvar7@gmail.com>
2021-11-10 13:40:43 +0100

    Cadenas

---
+++ 7_Cadenas/Cadenas.py
@@ -0,0 +1,11 @@
+# Metodos de cadenas: upper, lower, capitalize, count, find(indice de un caracter desde el principio), isdigit,
+# isalum, isalpha, split, strip (elimina espacios al principio y final), replace, rfind(indice de un caracter desde
+# el final r->reverse)
+
+frase = " hola Que Tal estas  "
+print(frase.capitalize())
+print(frase.upper())
+print(frase.lower())
+print(frase.find("Q"))
+print(frase.rfind("Q"))
+print(frase.strip())
